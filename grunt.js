13aa7fc5d202d5c4acbe4e4743336c5784266f64
davetayls <dave@the-taylors.org>
2012-11-22 13:33:15 +0000

    add grunt and min functionality

--- grunt.js
+++ grunt.js
@@ -5,11 +5,11 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: '<json:package.json>',
     meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+        ' Licensed <%= pkg.license %> */'
     },
     lint: {
       files: ['grunt.js', 'jquery.kinetic.js']
@@ -17,16 +17,10 @@ module.exports = function(grunt) {
     qunit: {
       files: ['test/**/*.html']
     },
-    concat: {
-      dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
     min: {
       dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
+        src: ['<banner:meta.banner>', 'jquery.kinetic.js'],
+        dest: 'jquery.kinetic.min.js'
       }
     },
     watch: {
@@ -55,6 +49,6 @@ module.exports = function(grunt) {
   });
 
   // Default task.
-  grunt.registerTask('default', 'lint concat min');
+  grunt.registerTask('default', 'lint min');
 
 };
